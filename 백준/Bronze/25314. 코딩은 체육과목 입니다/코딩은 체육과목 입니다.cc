c4e36ab98eddb952f24c4823c628a8aa062b4c07
Jeonmingyu <bgg01373@gmail.com>
2025-08-31 21:49:09 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num, number = 0;
+	cin >> num;
+	number = num / 4;
+	for (int i = 1; i <= number; i++) {
+		cout << "long ";
+	}
+	cout << "int";
+	return 0;
+}
