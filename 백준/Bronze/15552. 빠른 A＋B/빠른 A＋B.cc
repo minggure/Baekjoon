e3335631e9ff5d5358fb6a540c103ff8a8300814
Jeonmingyu <bgg01373@gmail.com>
2025-09-01 04:17:37 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 196 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int T,A,B= 0;
+
+
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		int sum = 0;
+		cin >> A >> B; 
+		cout << A + B << "\n";
+	}
+	return 0;
+}
+
