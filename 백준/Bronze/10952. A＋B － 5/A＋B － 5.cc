9cfa3d36b7e4dd8713eef4121692a7c0cb3dadf8
Jeonmingyu <bgg01373@gmail.com>
2025-09-05 03:55:51 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A, B = 0;
+  
+    while(true){
+        cin >> A >> B;
+        if (A == 0 && B == 0) {
+            break;
+        }
+        cout << A + B << endl;
+    }
+    return 0;
+}
+
