50581fb33b840944aa4f4a4e29d9abe175b5258d
Jeonmingyu <bgg01373@gmail.com>
2025-09-05 18:44:02 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int A = 0;
+    int B = 0;
+  
+    while(cin >> A >> B){
+        cout << A + B << endl;
+       
+    }
+    return 0;
+}
+
+
