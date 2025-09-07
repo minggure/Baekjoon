a7cbbf85e56501d5d080f9eac93deb97f2c498d1
Jeonmingyu <bgg01373@gmail.com>
2025-09-08 02:35:43 +0900

    [Bronze V] Title: A/B, Time: 260 ms, Memory: 17984 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1 = scanner.nextDouble();
+        double num2 = scanner.nextDouble();
+        System.out.print(num1/num2);
+
+    }
+}
