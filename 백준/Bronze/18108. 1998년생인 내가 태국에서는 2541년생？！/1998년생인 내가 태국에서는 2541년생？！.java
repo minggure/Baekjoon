8b4643e93a77223493e111ef70ed590f3bdfb06e
Jeonmingyu <bgg01373@gmail.com>
2026-02-17 22:53:15 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 192 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int num = scan.nextInt();
+    int age = num - 543;
+        System.out.println(age);
+    }
+}
