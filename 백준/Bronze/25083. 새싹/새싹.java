4571b497c63fd77280394679feb71bb1f4b77003
Jeonmingyu <bgg01373@gmail.com>
2026-02-27 00:14:06 +0900

    [Bronze V] Title: 새싹, Time: 168 ms, Memory: 17424 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+            System.out.println("         ,r'\"7\n" +
+                    "r`-_   ,'  ,/\n" +
+                    " \\. \". L_r'\n" +
+                    "   `~\\/\n" +
+                    "      |\n" +
+                    "      |");
+        }
+
+        }
+        
