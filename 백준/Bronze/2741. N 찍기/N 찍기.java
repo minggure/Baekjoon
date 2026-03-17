1c9675bf5b7ce5bbb67d3f33a6c2656720f6a07d
Jeonmingyu <bgg01373@gmail.com>
2026-03-17 22:46:23 +0900

    [Bronze V] Title: N 찍기, Time: 784 ms, Memory: 32660 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int num = 1;
+        for(int i = 0; i < N; i++) {
+            System.out.println(num);
+            num++;
+        }
+
+    }
+}
+
+
+
