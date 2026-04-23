b429e882cc9b74e8e61488ea769906a185e3e100
Jeonmingyu <bgg01373@gmail.com>
2026-04-23 13:46:52 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.54 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+
+        Arrays.sort(array);
+
+        answer = array[array.length/2];
+
+        return answer;
+    }
+}
