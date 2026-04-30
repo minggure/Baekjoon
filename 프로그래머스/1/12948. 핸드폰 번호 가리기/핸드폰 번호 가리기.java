e90537038f1a0555cb70680942f62929d455d527
Jeonmingyu <bgg01373@gmail.com>
2026-04-30 09:12:55 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.06 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder(phone_number);
+        for(int i = 0; i < sb.length() - 4; i++){
+            sb.setCharAt(i,'*');
+        }
+        return sb.toString();
+    }
+}
